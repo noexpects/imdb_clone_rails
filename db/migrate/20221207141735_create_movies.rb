7e47467f210db0ff0e2aef19ae9d0e821ddcbdd4
noexpects <abworkmailbox@gmail.com>
2022-12-07 17:19:33 +0200

    creaated book and category models

---
+++ db/migrate/20221207141735_create_movies.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title, null: false
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
